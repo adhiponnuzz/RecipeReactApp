57905b65e89a6db2bdf92e6f6db6dc4ad37823c4
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-29 01:00:38 +0530

    search finish

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Recipeentry from './component/Recipeentry';
+import Search from './component/Search';
 
 function App() {
   return (
     <>
     <Recipeentry/>
+    <Search/>
     
     
     </>
---
+++ src/component/Search.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const Search = () => {
+    var [title,setTitle]=useState("")
+    const subData=()=>{
+        const data={"title":title}
+        console.log(data)
+    }
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Recipe Title</label>
+                    <input onChange={(e)=>{setTitle(e.target.value)}} placeholder="Enter RecipeTitle" type="text" className="form-control"/>
+
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={subData} className="btn btn-primary">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Search
